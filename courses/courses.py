97eb091ccee6a74340624df30cb7d465312fabc9
Artur A. Galstyan <galstyan.artu@gmail.com>
2022-07-05 20:58:58 +0200

    added courses routing

--- courses/courses.py
+++ courses/courses.py
@@ -7,3 +7,11 @@ courses_bp = Blueprint("courses_bp", __name__, template_folder="templates")
 @courses_bp.route("/")
 def home():
     return render_template("courses/index.html")
+
+
+@courses_bp.route("feedforward_nn/")
+@courses_bp.route("feedforward_nn/<course_number>")
+def feedforward_nn(course_number=None):
+    if not course_number:
+        course_number = 1
+    return render_template(f"courses/feedforward_nn/{course_number}.html")
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from index.index import index_bp
-from courses.courses import blog_bp
+from courses.courses import courses_bp
 
 
 app = Flask(__name__)
 
 app.register_blueprint(index_bp, url_prefix="/")
-app.register_blueprint(blog_bp, url_prefix="/blog")
+app.register_blueprint(courses_bp, url_prefix="/courses")
